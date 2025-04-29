4ca82b1845d92905e6bf66c846cabe0291ec9fcc
paulomarchon <paulomarchon@gmail.com>
2025-04-29 00:38:35 -0300

    feat: Adicao do modulo domain e definicao de um ID base

---
+++ backend/src/main/java/com/connectacomuniade/domain/src/main/java/com/connectacomunidade/domain/BaseId.java
@@ -0,0 +1,28 @@
+package com.connectacomunidade.domain;
+
+import java.util.Objects;
+
+public abstract class BaseId<T> {
+    protected final T value;
+
+    protected BaseId(T value) {
+        Objects.requireNonNull(value);
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseId<?> baseId = (BaseId<?>) o;
+        return Objects.equals(value, baseId.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
